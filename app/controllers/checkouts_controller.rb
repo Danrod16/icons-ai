c12355b1a8d9fe398d77a3395427715d0fe81f55
Daniel Rodriguez <daniel.rod93@gmail.com>
2024-01-28 12:30:52 -0300

    added production ready product id

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,10 +1,11 @@
 class CheckoutsController < ApplicationController
    def show
         current_user.set_payment_processor :stripe
-
+        product_prod = "price_1OdaIZFhsXVBkTNCC7tVmpNZ"
+        product_test = "price_1ObpKFFhsXVBkTNCFnekdA0R"
         @checkout_session = current_user.payment_processor.checkout(
             mode: "payment",
-            line_items: "price_1ObpKFFhsXVBkTNCFnekdA0R"
+            line_items: product_prod
         )
     end     
 
