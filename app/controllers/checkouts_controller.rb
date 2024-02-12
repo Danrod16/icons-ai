ea829130271e0b6a06e9592589bac1c63a3d588c
Daniel Rodriguez <daniel.rod93@gmail.com>
2024-02-12 19:30:54 -0300

    added success_urls to stripe checkouts

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,11 +1,12 @@
 class CheckoutsController < ApplicationController
    def show
         current_user.set_payment_processor :stripe
-        product_prod = "price_1OdaIZFhsXVBkTNCC7tVmpNZ"
-        product_test = "price_1ObpKFFhsXVBkTNCFnekdA0R"
+        product = Rails.env.production? ? "price_1OdaIZFhsXVBkTNCC7tVmpNZ" : "price_1ObpKFFhsXVBkTNCFnekdA0R"
         @checkout_session = current_user.payment_processor.checkout(
             mode: "payment",
-            line_items: product_prod
+            line_items: product,
+            success_url: root_path,
+            cancel_url: root_path
         )
     end     
 
